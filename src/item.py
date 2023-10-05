5bcddfc9b529a3fba8f083390371164a16e81623
Anastasya Tomilova <hastya88.88@gmail.com>
2023-10-05 17:51:17 +0300

    Init

--- src/item.py
+++ src/item.py
@@ -8,23 +8,20 @@ class Item:
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
-
-        :param name: Название товара.
-        :param price: Цена за единицу товара.
-        :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name  # Название товара
+        self.price = price  # Цена за единицу товара
+        self.quantity = quantity  # Количество товара в магазине
+        self.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
-
-        :return: Общая стоимость товара.
         """
-        pass
+        return f"{self.price * self.quantity}"  # Общая стоимость товара
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price *= self.pay_rate
