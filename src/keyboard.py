2057a3e195bf531ce3610ce0ed1d9b189550dab5
Anastasya Tomilova <hastya88.88@gmail.com>
2023-10-19 21:12:07 +0300

    Rework 2 HW5

--- src/keyboard.py
+++ src/keyboard.py
@@ -1,15 +1,30 @@
 from src.item import Item
 
-
 class LanguageMixin:
-    def __init__(self):
-        self.language = "EN"
 
-    def change_lang(self):
-        if self.language == "EN":
-            self.language = "RU"
+    def __init__(self):
+        """Инциализации с языком по умолчанию EN."""
+        self.__language = 'EN'
+
+    @property
+    def language(self):
+        return self.__language
+
+    def change_lang(self) -> None:
+        """Метод для смены языка"""
+        if self.__language == 'EN':
+            self.__language = 'RU'
         else:
-            self.language = "EN"
+            self.__language = 'EN'
+        return self
+
+
+    @language.setter
+    def language(self, value):
+        if value in ["EN", "RU"]:
+            self.__language = value  # Fix the attribute name here
+        else:
+            raise ValueError("AttributeError: property 'language' of 'Keyboard' object has no setter")
 
 
 # class Keyboard(Item, LanguageMixin):
@@ -21,14 +36,3 @@ class Keyboard(Item, LanguageMixin):
     def __init__(self, name: str, price: float, quantity: int):
         super().__init__(name, price, quantity)
         LanguageMixin.__init__(self)
-
-    @property
-    def language(self):
-        return self.__language
-
-    @language.setter
-    def language(self, value):
-        if value in ["EN", "RU"]:
-            self.__language = value  # Fix the attribute name here
-        else:
-            raise ValueError("AttributeError: property 'language' of 'Keyboard' object has no setter")
